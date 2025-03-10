2ef89bcc8651ec9c90341e6eee0311809412b23a
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-03-10 15:16:55 +0100

    Calculator contract

---
+++ notes.md
@@ -0,0 +1,9 @@
+## Bloque 6 - Foundry
+
+Commands:
+
+forge init 
+forge build = compiles code
+forge test = executes tests
+forge test -vvvv = test details
+
---
+++ src/Calculator.sol
@@ -0,0 +1,51 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity >= 0.8.24;
+
+contract Calculator {
+  uint256 public result;
+  address public admin;
+  constructor(uint256 _result){
+    result = _result;
+    admin = msg.sender;
+  }
+
+  // events
+  event Operation(string operationType, uint256 n1, uint256 n2, uint256 result);
+
+  // modifiers
+  modifier onlyAdmin() {
+    require(msg.sender == admin, "only admin allowed");
+    _;
+  }
+
+  // functions
+
+  //1. Addition
+  function addition(uint256 n1, uint256 n2) external returns(uint256 _result){
+    _result = n1 + n2;
+    result = _result;
+
+    emit Operation("addition", n1, n2, result);
+  }
+  //2. Subtraction
+  function subtraction(uint256 n1, uint256 n2) external returns(uint256 _result) {
+    _result = n1 - n2;
+    result = _result;
+
+    emit Operation("substraction", n1, n2, result); 
+  }
+  //3. Multiplication
+  function multiplication(uint256 n1, uint256 n2) external returns(uint256 _result) {
+    _result = n1 * n2;
+    result = _result;
+
+    emit Operation("multiplication", n1, n2, result);
+  }
+  //4. Division
+  function division(uint256 n1, uint256 n2) external onlyAdmin returns(uint256 _result) {
+    _result = n1/ n2;
+    result = _result;
+
+    emit Operation("division", n1, n2, result);
+  }
+}
